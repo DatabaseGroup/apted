75ccb202662b61c9de975ed58fff3fcca0e13c11
Mateusz Pawlik <mpawlik@cosy.sbg.ac.at>
2017-03-06 23:16:49 +0100

    CommandLine.java: Temporarily disabled 'trees from file' and 'set custom costs'. Some formatting.

--- src/main/java/util/CommandLine.java
+++ src/main/java/util/CommandLine.java
@@ -31,6 +31,7 @@ import java.util.Scanner;
 import java.util.Vector;
 import distance.APTED;
 import node.Node;
+import node.StringNodeData;
 import costmodel.CostModel;
 import costmodel.StringUnitCostModel;
 import parser.InputParser;
@@ -46,95 +47,94 @@ import parser.BracketStringInputParser;
  */
 public class CommandLine<C extends CostModel, P extends InputParser> {
 
-	private String helpMessage =
-    		"\n" +
-    		"Compute the edit distance between two trees.\n" +
-    		"\n" +
-            "SYNTAX\n" +
-    		"\n" +
-    		"    java -jar APTED.jar {-t TREE1 TREE2 | -f FILE1 FILE2} [-c CD CI CR] [-v]\n" +
-    		"\n" +
-    		"    java -jar APTED.jar -h\n" +
-    		"\n" +
-    		"DESCRIPTION\n" +
-    		"\n" +
-    		"    Compute the edit distance between two trees with APTED algorithm [1,2].\n" +
-        "    APTED supersedes our RTED algorithm [3].\n" +
-        "    Currently only unit cost model is supported where each edit operation\n" +
-        "    has cost 1 (in case of equal labels the cost is 0).\n" +
-    		"\n" +
-        "LICENCE\n" +
-        "\n" +
-        "    The source code of this program is published under the MIT licence and\n" +
-        "    can be found on github (https://github.com/DatabaseGroup/apted).\n" +
-        "\n" +
-    		"OPTIONS\n" +
-    		"\n" +
-            "    -h, --help \n" +
-            "        print this help message.\n" +
-            "\n" +
-            "    -t TREE1 TREE2,\n" +
-            "    --trees TREE1 TREE2\n" +
-            "        compute the tree edit distance between TREE1 and TREE2. The\n" +
-            "        trees are encoded in the bracket notation, for example, in tree\n" +
-            "        {A{B{X}{Y}{F}}{C}} the root node has label A and two children\n" +
-            "        with labels B and C. B has three children with labels X, Y, F.\n" +
-            "\n" +
-            "    -f FILE1 FILE2, \n" +
-            "    --files FILE1 FILE2\n" +
-            "        compute the tree edit distance between the two trees stored in\n" +
-            "        the files FILE1 and FILE2. The trees are encoded in bracket\n" +
-            "        notation.\n" +
-            // "\n" +
-            // "    -c CD CI CR, \n" +
-            // "    --costs CD CI CR\n" +
-            // "        set custom cost for edit operations. Default is -c 1 1 1.\n" +
-            // "        CD - cost of node deletion\n" +
-            // "        CI - cost of node insertion\n" +
-            // "        CR - cost of node renaming\n" +
-            "\n" +
-            "    -v, --verbose\n" +
-            "        print verbose output, including tree edit distance, runtime,\n" +
-            "        number of relevant subproblems and strategy statistics.\n" +
-            "\n" +
-            "    -m, --mapping\n" +
-            "        compute the minimal edit mapping between two trees. There might\n" +
-            "        be multiple minimal edit mappings. This option computes only one\n" +
-            "        of them. The frst line of the output is the cost of the mapping.\n" +
-            "        The following lines represent the edit operations. n and m are\n" +
-            "        postorder IDs (beginning with 1) of nodes in the left-hand and\n" +
-            "        the rigt-hand trees respectively.\n" +
-            "            n->m - rename node n to m\n" +
-            "            n->0 - delete node n\n" +
-            "            0->m - insert node m\n" +
-    		"EXAMPLES\n" +
-    		"\n" +
-    		"    java -jar APTED.jar -t {a{b}{c}} {a{b{d}}}\n" +// -c 1 1 0.5\n" +
-    		"    java -jar APTED.jar -f 1.tree 2.tree\n" +
-    		// "    java -jar APTED.jar -t {a{b}{c}} {a{b{d}}} -v\n" +
-    		"\n" +
-    		"REFERENCES\n" +
-    		"\n" +
-    		"    [1] M. Pawlik and N. Augsten. Efficient Computation of the Tree Edit\n" +
-        "        Distance. ACM Transactions on Database Systems (TODS) 40(1). 2015.\n" +
-        "    [2] M. Pawlik and N. Augsten. Tree edit distance: Robust and memory-\n" +
-        "        efficient. Information Systems 56. 2016.\n" +
-        "    [3] M. Pawlik and N. Augsten. RTED: A Robust Algorithm for the Tree Edit\n" +
-        "        Distance. PVLDB 5(4). 2011.\n" +
-    		"\n" +
-    		"AUTHORS\n" +
-    		"\n" +
-    		"    Mateusz Pawlik, Nikolaus Augsten";
+  private String helpMessage =
+      "\n" +
+      "Compute the edit distance between two trees.\n" +
+      "\n" +
+      "SYNTAX\n" +
+      "\n" +
+      "    java -jar APTED.jar {-t TREE1 TREE2 | -f FILE1 FILE2} [-c CD CI CR] [-v]\n" +
+      "\n" +
+      "    java -jar APTED.jar -h\n" +
+      "\n" +
+      "DESCRIPTION\n" +
+      "\n" +
+      "    Compute the edit distance between two trees with APTED algorithm [1,2].\n" +
+      "    APTED supersedes our RTED algorithm [3].\n" +
+      "    Currently only unit cost model is supported where each edit operation\n" +
+      "    has cost 1 (in case of equal labels the cost is 0).\n" +
+      "\n" +
+      "LICENCE\n" +
+      "\n" +
+      "    The source code of this program is published under the MIT licence and\n" +
+      "    can be found on github (https://github.com/DatabaseGroup/apted).\n" +
+      "\n" +
+      "OPTIONS\n" +
+      "\n" +
+      "    -h, --help \n" +
+      "        print this help message.\n" +
+      "\n" +
+      "    -t TREE1 TREE2,\n" +
+      "    --trees TREE1 TREE2\n" +
+      "        compute the tree edit distance between TREE1 and TREE2. The\n" +
+      "        trees are encoded in the bracket notation, for example, in tree\n" +
+      "        {A{B{X}{Y}{F}}{C}} the root node has label A and two children\n" +
+      "        with labels B and C. B has three children with labels X, Y, F.\n" +
+      "\n" +
+      "    -f FILE1 FILE2, \n" +
+      "    --files FILE1 FILE2\n" +
+      "        compute the tree edit distance between the two trees stored in\n" +
+      "        the files FILE1 and FILE2. The trees are encoded in bracket\n" +
+      "        notation.\n" +
+      // "\n" +
+      // "    -c CD CI CR, \n" +
+      // "    --costs CD CI CR\n" +
+      // "        set custom cost for edit operations. Default is -c 1 1 1.\n" +
+      // "        CD - cost of node deletion\n" +
+      // "        CI - cost of node insertion\n" +
+      // "        CR - cost of node renaming\n" +
+      "\n" +
+      "    -v, --verbose\n" +
+      "        print verbose output, including tree edit distance, runtime,\n" +
+      "        number of relevant subproblems and strategy statistics.\n" +
+      "\n" +
+      "    -m, --mapping\n" +
+      "        compute the minimal edit mapping between two trees. There might\n" +
+      "        be multiple minimal edit mappings. This option computes only one\n" +
+      "        of them. The frst line of the output is the cost of the mapping.\n" +
+      "        The following lines represent the edit operations. n and m are\n" +
+      "        postorder IDs (beginning with 1) of nodes in the left-hand and\n" +
+      "        the rigt-hand trees respectively.\n" +
+      "            n->m - rename node n to m\n" +
+      "            n->0 - delete node n\n" +
+      "            0->m - insert node m\n" +
+      "EXAMPLES\n" +
+      "\n" +
+      "    java -jar APTED.jar -t {a{b}{c}} {a{b{d}}}\n" +// -c 1 1 0.5\n" +
+      "    java -jar APTED.jar -f 1.tree 2.tree\n" +
+      // "    java -jar APTED.jar -t {a{b}{c}} {a{b{d}}} -v\n" +
+      "\n" +
+      "REFERENCES\n" +
+      "\n" +
+      "    [1] M. Pawlik and N. Augsten. Efficient Computation of the Tree Edit\n" +
+      "        Distance. ACM Transactions on Database Systems (TODS) 40(1). 2015.\n" +
+      "    [2] M. Pawlik and N. Augsten. Tree edit distance: Robust and memory-\n" +
+      "        efficient. Information Systems 56. 2016.\n" +
+      "    [3] M. Pawlik and N. Augsten. RTED: A Robust Algorithm for the Tree Edit\n" +
+      "        Distance. PVLDB 5(4). 2011.\n" +
+      "\n" +
+      "AUTHORS\n" +
+      "\n" +
+      "    Mateusz Pawlik, Nikolaus Augsten";
 
   // [TODO] Review if all fields are necessary.
-	private String wrongArgumentsMessage = "Wrong arguments. Try \"java -jar RTED.jar --help\" for help.";
-	private LblTree lt1, lt2;
-	// private int size1, size2;
-	private boolean run, custom, array, strategy, ifSwitch, sota, verbose, demaine, mapping;
-	private int sotaStrategy;
-	private String customStrategy, customStrategyArrayFile;
-	private APTED rted;
-	private double ted;
+  private String wrongArgumentsMessage = "Wrong arguments. Try \"java -jar RTED.jar --help\" for help.";
+
+  private boolean run, custom, array, strategy, ifSwitch, sota, verbose, demaine, mapping;
+  private int sotaStrategy;
+  private String customStrategy, customStrategyArrayFile;
+  private APTED rted;
+  private double ted;
 
   private C costModel;
   private P inputParser;
@@ -155,69 +155,70 @@ public class CommandLine<C extends CostModel, P extends InputParser> {
     this.inputParser = inputParser;
   }
 
-	/**
-	 * Main method, invoced when executing the jar file.
-	 *
-	 * @param args array of command line arguments passed when executing jar file.
-	 */
-	public static void main(String[] args) {
-		CommandLine<StringUnitCostModel, BracketStringInputParser> rtedCL = new CommandLine<>(new StringUnitCostModel(), new BracketStringInputParser());
-		rtedCL.runCommandLine(args);
-	}
+  /**
+   * Main method, invoced when executing the jar file.
+   *
+   * @param args array of command line arguments passed when executing jar file.
+   */
+  public static void main(String[] args) {
+    CommandLine<StringUnitCostModel, BracketStringInputParser> rtedCL = new CommandLine<>(new StringUnitCostModel(), new BracketStringInputParser());
+    rtedCL.runCommandLine(args);
+  }
 
-	/**
-	 * Run the command line with given arguments.
-	 *
-	 * @param args array of command line arguments passed when executing jar file.
-	 */
-	public void runCommandLine(String[] args) {
-		rted = new APTED(1, 1, 1);
+  /**
+   * Run the command line with given arguments.
+   *
+   * @param args array of command line arguments passed when executing jar file.
+   */
+   public void runCommandLine(String[] args) {
+    rted = new APTED<C, StringNodeData>(1, 1, 1, costModel);
+    try {
+      for (int i = 0; i < args.length; i++) {
+        if (args[i].equals("--help") || args[i].equals("-h")) {
+          System.out.println(helpMessage);
+          System.exit(0);
+        } else if (args[i].equals("-t") || args[i].equals("--trees")) {
+          parseTreesFromCommandLine(args[i+1], args[i+2]);
+          i = i+2;
+          run = true;
+        // [TODO] -f option temporarily disabled for refactoring.
+        // } else if (args[i].equals("-f") || args[i].equals("--files")) {
+        //   parseTreesFromFiles(args[i+1], args[i+2]);
+        //   i = i+2;
+        //   run = true;
+        // } else if (args[i].equals("-c") || args[i].equals("--costs")) {
+        //   setCosts(args[i+1], args[i+2], args[i+3]);
+        //   i = i+3;
+        } else if (args[i].equals("-v") || args[i].equals("--verbose")) {
+          verbose = true;
+        } else if (args[i].equals("-m") || args[i].equals("--mapping")) {
+          mapping = true;
+        } else {
+          System.out.println(wrongArgumentsMessage);
+          System.exit(0);
+        }
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Too few arguments.");
+      System.exit(0);
+    }
 
-		try {
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].equals("--help") || args[i].equals("-h")) {
-				System.out.println(helpMessage);
-				System.exit(0);
-			} else if (args[i].equals("-t") || args[i].equals("--trees")) {
-				parseTreesFromCommandLine(args[i+1], args[i+2]);
-				i = i+2;
-				run = true;
-			} else if (args[i].equals("-f") || args[i].equals("--files")) {
-				parseTreesFromFiles(args[i+1], args[i+2]);
-				i = i+2;
-				run = true;
-			// } else if (args[i].equals("-c") || args[i].equals("--costs")) {
-			// 	setCosts(args[i+1], args[i+2], args[i+3]);
-			// 	i = i+3;
-			} else if (args[i].equals("-v") || args[i].equals("--verbose")) {
-				verbose = true;
-      } else if (args[i].equals("-m") || args[i].equals("--mapping")) {
-        mapping = true;
-			} else {
-				System.out.println(wrongArgumentsMessage);
-				System.exit(0);
-			}
-		}
+    if (!run) {
+      System.out.println(wrongArgumentsMessage);
+      System.exit(0);
+    }
 
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Too few arguments.");
-            System.exit(0);
-		}
+    long time1 = (new Date()).getTime();
 
-		if (!run) {
-			System.out.println(wrongArgumentsMessage);
-			System.exit(0);
-		}
+    ted = rted.nonNormalizedTreeDist(t1, t2);
 
-		long time1 = (new Date()).getTime();
-		ted = rted.nonNormalizedTreeDist(lt1, lt2);
-		long time2 = (new Date()).getTime();
-		if (verbose) {
-			System.out.println("distance:             " + ted);
-			System.out.println("runtime:              " + ((time2 - time1) / 1000.0));
-		} else {
-			System.out.println(ted);
-		}
+    long time2 = (new Date()).getTime();
+    if (verbose) {
+      System.out.println("distance:             " + ted);
+      System.out.println("runtime:              " + ((time2 - time1) / 1000.0));
+    } else {
+      System.out.println(ted);
+    }
 
     if (mapping) { // TED is computed anyways.
       LinkedList<int[]> editMapping = rted.computeEditMapping();
@@ -226,7 +227,7 @@ public class CommandLine<C extends CostModel, P extends InputParser> {
       }
     }
 
-	}
+  }
 
   /**
    * Parse two input trees from the command line and convert them to tree
@@ -239,62 +240,57 @@ public class CommandLine<C extends CostModel, P extends InputParser> {
   private void parseTreesFromCommandLine(String ts1, String ts2) {
     try {
       t1 = inputParser.fromString(ts1);
-      // size1 = NodeUtilities.getNodeCount(t1);
-      lt1 = LblTree.fromString(ts1);
-      // size1 = lt1.getNodeCount();
     } catch (Exception e) {
       System.out.println("TREE1 argument has wrong format");
       System.exit(0);
     }
     try {
       t2 = inputParser.fromString(ts2);
-      // size2 = NodeUtilities.getNodeCount(t2);
-      lt2 = LblTree.fromString(ts2);
-      // size2 = lt2.getNodeCount();
     } catch (Exception e) {
       System.out.println("TREE2 argument has wrong format");
       System.exit(0);
     }
 	}
 
-  /**
-   * Parses two input trees from given files.
-   *
-   * @param fs1 path to file with source tree.
-   * @param fs2 path to file with destination tree.
-   */
-  private void parseTreesFromFiles(String fs1, String fs2) {
-    try {
-      lt1 = LblTree.fromString((new BufferedReader(new FileReader(fs1))).readLine());
-      // size1 = lt1.getNodeCount();
-    } catch (Exception e) {
-      System.out.println("TREE1 argument has wrong format");
-      System.exit(0);
-    }
-    try {
-      lt2 = LblTree.fromString((new BufferedReader(new FileReader(fs2))).readLine());
-      // size2 = lt2.getNodeCount();
-    } catch (Exception e) {
-      System.out.println("TREE2 argument has wrong format");
-      System.exit(0);
-    }
-  }
+  // [TODO] Bring the functionalitites below back to life.
+  // /**
+  //  * Parses two input trees from given files.
+  //  *
+  //  * @param fs1 path to file with source tree.
+  //  * @param fs2 path to file with destination tree.
+  //  */
+  // private void parseTreesFromFiles(String fs1, String fs2) {
+  //   try {
+  //     lt1 = LblTree.fromString((new BufferedReader(new FileReader(fs1))).readLine());
+  //     // size1 = lt1.getNodeCount();
+  //   } catch (Exception e) {
+  //     System.out.println("TREE1 argument has wrong format");
+  //     System.exit(0);
+  //   }
+  //   try {
+  //     lt2 = LblTree.fromString((new BufferedReader(new FileReader(fs2))).readLine());
+  //     // size2 = lt2.getNodeCount();
+  //   } catch (Exception e) {
+  //     System.out.println("TREE2 argument has wrong format");
+  //     System.exit(0);
+  //   }
+  // }
 
-  /**
-   * Set custom costs for the edit operations.
-   *
-   * @deprecated
-   * @param cds cost of deletion.
-   * @param cis cost of insertion.
-   * @param cms cost of rename (mapping).
-   */
-   private void setCosts(String cds, String cis, String cms) {
-     try {
-       rted.setCustomCosts(Float.parseFloat(cds), Float.parseFloat(cis), Float.parseFloat(cms));
-     } catch (Exception e) {
-       System.out.println("One of the costs has wrong format.");
-       System.exit(0);
-     }
-  }
+  // /**
+  //  * Set custom costs for the edit operations.
+  //  *
+  //  * @deprecated
+  //  * @param cds cost of deletion.
+  //  * @param cis cost of insertion.
+  //  * @param cms cost of rename (mapping).
+  //  */
+  //  private void setCosts(String cds, String cis, String cms) {
+  //    try {
+  //      rted.setCustomCosts(Float.parseFloat(cds), Float.parseFloat(cis), Float.parseFloat(cms));
+  //    } catch (Exception e) {
+  //      System.out.println("One of the costs has wrong format.");
+  //      System.exit(0);
+  //    }
+  // }
 
 }
